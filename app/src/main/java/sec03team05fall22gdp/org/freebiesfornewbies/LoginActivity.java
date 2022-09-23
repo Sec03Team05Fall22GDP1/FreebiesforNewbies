f5cf730dc8be729964c1a0eb0e9d7953eb45ca46
Adithyakrishna9 <98135664+Adithyakrishna9@users.noreply.github.com>
2022-09-23 17:35:20 -0500

    Added the logic of the pop-up window.

--- app/src/main/java/sec03team05fall22gdp/org/freebiesfornewbies/LoginActivity.java
+++ app/src/main/java/sec03team05fall22gdp/org/freebiesfornewbies/LoginActivity.java
@@ -99,6 +99,32 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    public void onButtonShowPopupWindowClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+
     @Override
     protected void onStart() {
         super.onStart();

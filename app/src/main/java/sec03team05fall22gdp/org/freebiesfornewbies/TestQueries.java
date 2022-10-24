f9aa84100f69b08dd87f44e9301cb64fd1fc600d
Harish6600 <98137725+Harish6600@users.noreply.github.com>
2022-10-24 11:15:56 -0500

    Added ParseObject read query(Tested Ok).

--- app/src/main/java/sec03team05fall22gdp/org/freebiesfornewbies/TestQueries.java
+++ app/src/main/java/sec03team05fall22gdp/org/freebiesfornewbies/TestQueries.java
@@ -1,7 +1,11 @@
 package sec03team05fall22gdp.org.freebiesfornewbies;
 
+import android.util.Log;
+
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
 import java.util.Arrays;
@@ -22,8 +26,28 @@ public class TestQueries {
             public void done(ParseException e) {
                 if (e == null) {
                     // Success
+                    Log.v("Create Object:","Successful");
                 } else {
-                    // Error
+                    Log.v("Create Object:","failed due to ParseException "+e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void readParseObjects(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("SoccerPlayers");
+        query.whereEqualTo("objectId", "HMcTr9rD3s");
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            public void done(ParseObject player, ParseException e) {
+                if (e == null) {
+                    String playerName = player.getString("playerName");
+                    int yearOfBirth = player.getInt("yearOfBirth");
+                    String emailPlayer =  player.getString("emailContact");
+                    Log.v("playerName",playerName);
+                    Log.v("yearOfBirth",String.valueOf(yearOfBirth));
+                    Log.v("emailContact",emailPlayer);
+                } else {
+                    Log.v("ParseException",e.getMessage());
                 }
             }
         });

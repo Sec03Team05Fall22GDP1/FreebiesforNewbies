7aa52ee7d7fa73ff39e793769596e84b58daedf1
manoj2205 <manojgude1999@gmail.com>
2022-10-24 09:15:10 -0500

    UpdateEventActivity layout design initial commit to handle update operations

---
+++ app/src/main/java/sec03team05fall22gdp/org/freebiesfornewbies/UpdateEventActivity.java
@@ -0,0 +1,14 @@
+package sec03team05fall22gdp.org.freebiesfornewbies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_event);
+    }
+}

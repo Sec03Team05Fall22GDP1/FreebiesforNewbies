49ddda301774f53029d1b90b8a8d3ad223ebb358
Adithyakrishna9 <98135664+Adithyakrishna9@users.noreply.github.com>
2022-10-24 22:51:11 -0500

    DetailedEventActivity layout design initial commit for detailed View

---
+++ app/src/main/java/sec03team05fall22gdp/org/freebiesfornewbies/DetailedEventActivity.java
@@ -0,0 +1,14 @@
+package sec03team05fall22gdp.org.freebiesfornewbies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailedEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_event);
+    }
+}

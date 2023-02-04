fa2a7fda536d2aade77c3ae576d55e821acd702d
Harish6600 <98137725+Harish6600@users.noreply.github.com>
2023-02-03 22:17:18 -0600

    Added few methods in AdminEventHomeActivity java class.

--- app/src/main/java/sec03team05fall22gdp/org/freebiesfornewbies/AdminEventHomeActivity.java
+++ app/src/main/java/sec03team05fall22gdp/org/freebiesfornewbies/AdminEventHomeActivity.java
@@ -13,8 +13,10 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -270,3 +272,187 @@ public class AdminEventHomeActivity extends AppCompatActivity {
 });
 
         }
+
+private boolean setUpEventModels() {
+        // Read Parse Objects
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Events");
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy", Locale.getDefault());
+        String formattedDate = df.format(date);
+
+        Date dateToday = null;
+        try {
+        dateToday = new SimpleDateFormat("MM-dd-yyyy").parse(formattedDate);
+        } catch (java.text.ParseException e) {
+        e.printStackTrace();
+        }
+        Log.v("today's Date: ", dateToday.toString());
+        calendar.setTime(dateToday);
+        calendar.add(Calendar.DATE, 1);
+        Date dateTomorrow = calendar.getTime();
+        Log.v("tomorrow's Date: ", dateTomorrow.toString());
+        query.whereGreaterThanOrEqualTo("eventStartDt", dateToday).whereLessThan("eventStartDt", dateTomorrow).whereEqualTo("isApproved", Boolean.TRUE);
+        query.findInBackground(new FindCallback<ParseObject>() {
+@Override
+public void done(List<ParseObject> results, ParseException e) {
+        for (ParseObject eventObj : results) {
+        if (e == null) {
+        String eventID, eventName, eventStDT, eventEndDt, eventDescription,eventAddressLine1,
+        eventAddressLine2, eventCity,eventState, eventCountry, eventZipcode, eventNotes;
+        eventID= eventObj.getObjectId();
+        eventName = eventObj.getString("eventName");
+        eventStDT = String.valueOf(eventObj.getDate("eventStartDt"));
+        eventEndDt =  String.valueOf(eventObj.getDate("eventEndDt"));
+        eventDescription =  eventObj.getString("eventDescription");
+        eventAddressLine1 =  eventObj.getString("eventAddressLine1") ;
+        eventAddressLine2 =  eventObj.getString("eventAddressLine2");
+        eventCity= eventObj.getString("eventCity") ;
+        eventState= eventObj.getString("eventState") ;
+        eventCountry= eventObj.getString("eventCountry");
+        eventZipcode= eventObj.getString("eventZipcode") ;
+        eventNotes =  eventObj.getString("eventNotes");
+        Log.v("ObjectID",String.valueOf(eventID));
+
+        myEModel.eventsList.add(new EventModel.Events(eventID,eventName,eventStDT,eventEndDt,eventDescription,eventAddressLine1,eventAddressLine2,eventCity,eventState,eventCountry,eventZipcode,eventNotes));
+
+        Log.v("Setup EventList Size:", String.valueOf(myEModel.eventsList.size()));
+        adapter = new EventRAdapter(HomeActivity.this, myEModel);
+        Log.v("adapter", String.valueOf(adapter.getItemCount()));
+
+        recyclerView = findViewById(R.id.eventRecyclerView);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(HomeActivity.this));
+
+        detector = new GestureDetectorCompat(HomeActivity.this, new RecyclerViewOnGestureListener());
+        recyclerView.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener(){
+@Override
+public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        return detector.onTouchEvent(e);
+        }
+        });
+        } else {
+        Toast.makeText(HomeActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        }
+        }
+        });
+
+
+        return true;
+        }
+
+private void showAlert(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this)
+        .setTitle(title)
+        .setMessage(message)
+        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+@Override
+public void onClick(DialogInterface dialog, int which) {
+        dialog.cancel();
+        // don't forget to change the line below with the names of your Activities
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        }
+        });
+        AlertDialog ok = builder.create();
+        ok.show();
+        }
+
+public void openDatePicker(View view){
+        datePickerDialog.show();
+        }
+
+private void initDatePicker()    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+@Override
+public void onDateSet(DatePicker datePicker, int year, int month, int day)
+        {
+        month = month + 1;
+        String date = makeDateString(day, month, year);
+        dateButton.setText(date);
+        }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(this, android.R.style.Holo_Light_ButtonBar_AlertDialog, dateSetListener, year, month, day);
+        //datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+        }
+
+private String makeDateString(int day, int month, int year){
+        return getMonthFormat(month) + " " + day + " " + year;
+        }
+
+private String getTodaysDate()    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+        }
+
+private String getMonthFormat(int month){
+        if(month == 1)
+        return "Jan";
+        if(month == 2)
+        return "Feb";
+        if(month == 3)
+        return "Mar";
+        if(month == 4)
+        return "Apr";
+        if(month == 5)
+        return "May";
+        if(month == 6)
+        return "Jun";
+        if(month == 7)
+        return "Jul";
+        if(month == 8)
+        return "Aug";
+        if(month == 9)
+        return "Sep";
+        if(month == 10)
+        return "Oct";
+        if(month == 11)
+        return "Nov";
+        if(month == 12)
+        return "Dec";
+
+        //default should never happen
+        return "JAN";
+        }
+
+private class RecyclerViewOnGestureListener extends GestureDetector.SimpleOnGestureListener {
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        View view = recyclerView.findChildViewUnder(e.getX(), e.getY());
+        if (view != null) {
+            RecyclerView.ViewHolder holder = recyclerView.getChildViewHolder(view);
+            if (holder instanceof EventRAdapter.MyViewHolder) {
+                int position = holder.getAdapterPosition();
+                // handle single tap
+                String sEventId= myEModel.eventsList.get(position).eventID;
+                Log.v("Selected EventID: ",sEventId);
+
+                Intent intent = new Intent(HomeActivity.this, DetailedEventActivity.class);
+                intent.putExtra("eventID",sEventId);
+                startActivity(intent);
+                return true; // Use up the tap gesture
+            }
+        }            // we didn't handle the gesture so pass it on
+        return false;
+    }
+}
+
+
+
+
+

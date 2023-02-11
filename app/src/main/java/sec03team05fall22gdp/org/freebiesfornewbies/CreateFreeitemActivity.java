4e56a7750349af623651b9fca58f2a28d7c31afc
manoj2205 <manojgude1999@gmail.com>
2023-02-10 20:01:53 -0600

    Add and Create create free item activity for free items

---
+++ app/src/main/java/sec03team05fall22gdp/org/freebiesfornewbies/CreateFreeitemActivity.java
@@ -0,0 +1,14 @@
+package sec03team05fall22gdp.org.freebiesfornewbies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateFreeitemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_freeitem);
+    }
+}
